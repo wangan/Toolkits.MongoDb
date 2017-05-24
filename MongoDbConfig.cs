a4cae60362d746072c0f18425ca79e5d1c114cfd
wangan <anntony.wang@live.com>
2017-05-24 14:22:17 +0800

    Add project files.

---
+++ MongoDbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkits.MongoDb {
+    public abstract class MongoDbConfig : ConfigBase {
+        public static string MongoDbConn { get; set; }
+
+        static MongoDbConfig() {
+            MongoDbConn = GetConfigInfo<string>("MongoDbConn", "");
+        }
+    }
+}
---
+++ MongoDbHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MongoDB.Driver;
+using Toolkits.Log;
+
+namespace Toolkits.MongoDb {
+
+    /// <summary> 
+    /// </summary>
+    public class MongoDbHelper {
+        private static MongoDbHelper _Instance = new MongoDbHelper();
+        private static MongoClient _Client;
+
+        public MongoDbHelper() {
+            if (null == _Client) {
+                try {
+                    var connStr = MongoDbConfig.MongoDbConn;
+                    if (!string.IsNullOrEmpty(connStr)) {
+                        _Client = new MongoClient(connStr);
+                    }
+                } catch (Exception ex) {
+                    LogHelper.Instance.Error("[MongoDbHelper]", ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 全局单例
+        /// </summary>
+        public static MongoDbHelper Instance {
+            get {
+                if (_Instance == null) {
+                    _Instance = new MongoDbHelper();
+                }
+                return _Instance;
+            }
+        }
+
+        public void ReConstruct() {
+            _Instance = null;
+        }
+
+        public IMongoDatabase GetDb(string dbName = "") {
+            IMongoDatabase db = null;
+            try {
+                db = _Client.GetDatabase(dbName);
+            } catch (Exception ex) {
+                LogHelper.Instance.Error("[MongoDbHelper][GetDb]", ex);
+
+                ReConstruct();
+
+                LogHelper.Instance.Warn("[MongoDbHelper][GetDb]: 重建到Redis的连接");
+            }
+
+            return db;
+        }
+    }
+}
